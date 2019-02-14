1e9193c631f80b300085de84584b934fc4a29b3c
crossaaron <atcbcrossx@gmail.com>
2019-02-14 07:07:52 -0800

    initial component build

--- src/App.js
+++ src/App.js
@@ -1,36 +1,8 @@
 import React, { Component } from 'react';
-
 import './App.css';
-
-class Status extends Component {
-    constructor () {
-        super();
-        this.state = {
-            likes: 0
-      }
-      this.like = this.like.bind(this);
-  }
-
-  like() {
-        this.setState({
-            likes: this.state.likes + 1
-        });
-  }
+import Status from './Components/Status'
 
 
-    render() {
-    return (
-        <div className='status'>
-            <p>{this.props.mytext}</p>
-            <p>
-                <button onClick={this.like}>
-                    {this.state.likes} Likes
-                </button>
-            </p>
-        </div>
-    )
-  }
-}
 
 class App extends Component {
   render() {
